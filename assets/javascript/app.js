6041ba153479c302ca49d3c81021942c8dd498f5
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-03-12 23:08:26 -0400

    gif display buttons working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,37 +9,59 @@ $(document).ready(function() {
     // Declare functions
     var gif = {
         
-        // Add item to buttons array
+        // Add item to buttons section
         addItemButton: function() {
             event.preventDefault();
             newTopic = $("#addItem").val().trim();
             topics.push(newTopic);
-            console.log(topics);
             $("#addItem").val("");
-            $("#gifDisplay").text(newTopic);
+            gif.displayButtons();
         },
 
         // Generate buttons from topics array elements
         displayButtons: function() {
+            
+            // Clear the previous set of buttons
+            $("#buttonDisplay").empty();
+
+            // Display new set of buttons
             for (i = 0; i < topics.length; i++) {
                 var gifButton = $("<button>").text(topics[i]);
-                gifButton.addClass("btn btn-info");
+                gifButton.addClass("btn btn-info btnGif");
                 gifButton.attr("data-topic", topics[i]);
                 $("#buttonDisplay").append(gifButton);
             }
-        }
-
-
+        },
 
         // Display gifs on screen
-
+        displayGifs: function() {
+            var gifTopicToGet = $(this).attr("data-topic");
+            $.ajax({
+                url: queryURL + gifTopicToGet,
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+                var giphyArray = response.data;
+                for (i = 0; i < giphyArray.length; i++) {
+                    var thumbnail = $("<span>");
+                    var thumbnailImg = $("<img>").addClass("img-thumbnail");
+                    thumbnailImg.attr("src", giphyArray[i].images.fixed_height_still.url);
+                    var thumbnailCap = $("<em>Rating: " + giphyArray[i].rating + "</em>");
+                    thumbnailImg.append(thumbnailCap);
+                    thumbnail.html(thumbnailImg);
+                    $("#gifDisplay").append(thumbnail);
+                }
+            });
+        }
     };
 
     // Execute functions
-
     // Display initial button set on page load
     gif.displayButtons();
 
     // Submit button to add new topic
     $("#submitButton").on("click", gif.addItemButton);
+
+    // Gif button to display gifs
+    $(document).on("click", ".btnGif", gif.displayGifs);
 });
